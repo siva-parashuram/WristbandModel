8a406bf85449c562d7121ee66a4642567d97d261
SIVA <SIVA@RFIDSOFTWARE1>
2020-08-18 14:17:18 +0530

    Add project files.

---
+++ Wristband/Controllers/WristModelController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Wristband.Models;
+
+namespace Wristband.Controllers
+{
+    public class WristModelController : Controller
+    {
+        WristbandModel data = new WristbandModel();
+        public IActionResult Index()
+        {
+             
+            var lstColors = new List<string>() { "red", "yellow" ,  "black", "white", "green", "pink" };
+            data.colors = lstColors;
+ 
+            var lstfrmimg = new List<string>() { "pat1.jpg", "pat2.jpg", "pat3.jpg" };
+            data. frmimg = lstfrmimg;
+
+           
+             
+            return View(data);
+           
+        }
+    }
+}
---
+++ Wristband/Models/WristbandModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Wristband.Models
+{
+    public class WristbandModel
+    {
+        public int ID { get; set; }
+        public string  productcode { get; set; }
+        public string productname { get; set; }
+        public double  price { get; set; }
+        public List<string> colors = new List<string>();
+        public List<string> btnimg = new List<string>();
+        public List<string> frmimg = new List<string>();
+    }
+}
